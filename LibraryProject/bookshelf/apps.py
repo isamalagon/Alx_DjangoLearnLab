1048025bbf1aeed1df159bb92b94eb3440b48050
Isa Malagon <isabelmalagon2010@gmail.com>
2025-11-02 15:56:05 -0500

    Update settings.py and register catalog app

--- LibraryProject/bookshelf/apps.py
+++ LibraryProject/bookshelf/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BookshelfConfig(AppConfig):
+class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookshelf'
+    name = 'catalog'
