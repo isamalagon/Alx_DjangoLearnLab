4e00675327ff99f12e2a4e693ccbc48926dec01c
Isa Malagon <isabelmalagon2010@gmail.com>
2025-11-02 15:36:01 -0500

    Add Book model to bookshelf/models.py

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
