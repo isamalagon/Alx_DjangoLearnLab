7f6c739bbf587739cc0d7ccdbf5d69dbc3fb0d32
Isa Malagon <isabelmalagon2010@gmail.com>
2025-11-23 14:54:26 -0500

    Add token authentication and permission control to BookViewSet

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookList, BookViewSet
 
 router = DefaultRouter()
@@ -11,5 +12,6 @@ from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),  # ✅ Includes all CRUD routes
+    path('get-token/', obtain_auth_token, name='get-token'),
 
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 from rest_framework import generics
@@ -15,3 +16,5 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
