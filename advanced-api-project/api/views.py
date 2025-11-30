a9a6647f9069df5a6860c1188a58e260d9558380
Isa Malagon <isabelmalagon2010@gmail.com>
2025-11-30 16:26:17 -0500

    Add Book CRUD views, urls, permissions, and docs

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
