cca83375c2bad7b9f20904d85fa4bec2200a1e27
Isa Malagon <isabelmalagon2010@gmail.com>
2025-11-30 17:03:15 -0500

    Configure OrderingFilter for Book ListView with title and publication_year

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
