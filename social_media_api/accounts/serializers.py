5b19014d04ed7be92377e9a1eea67069d3669e86
Isa Malagon <isabelmalagon2010@gmail.com>
2025-12-13 17:56:48 -0500

    Fix: add Token and get_user_model logic for checker compliance

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,15 +13,19 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_User_model()
         fields = ('username','email','password')
 
     def create(self, validated_data):
-        return User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
         )
+        Token.objects.create(user=user) 
+        return user
+
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
