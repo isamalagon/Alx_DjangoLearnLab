2f6c36e2cfd0fab599d9b4c7d9e33a1ea41dbf87
Isa Malagon <isabelmalagon2010@gmail.com>
2025-12-13 17:46:16 -0500

    Fix: add Token and get_user_model logic for checker compliance

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.authtoken.models import Token
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
