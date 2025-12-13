b3e8b7a1849e12a5acf74c3cc5f42e049d511132
Isa Malagon <isabelmalagon2010@gmail.com>
2025-12-13 18:13:54 -0500

    Add posts and comments functionality with models, views, serializers, and routing

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -51,7 +51,12 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ),
+        ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.SearchFilter'],
+
+
 }
 
 
