a7b855e2bfc5ceacc5aed8bb3d3dc071281fca82
Isa Malagon <isabelmalagon2010@gmail.com>
2025-12-13 19:50:33 -0500

    Add Like model, Notification model, like/unlike views and URLs

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,16 +1,6 @@
 # Create your models here.
 from django.db import models
 from django.conf import settings
-from .models import Post
-
-class Like(models.Model):
-    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='likes', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('post', 'user')
-
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -26,4 +16,10 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('post', 'user')
