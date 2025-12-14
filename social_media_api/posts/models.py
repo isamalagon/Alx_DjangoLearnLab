b5b8322ea9784388807aff633e51c9fc151e6df4
Isa Malagon <isabelmalagon2010@gmail.com>
2025-12-13 19:36:40 -0500

    Add Like model, notifications app, like/unlike endpoints, and notifications feed

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,8 +1,16 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
 from django.conf import settings
+from .models import Post
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -17,3 +25,5 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
